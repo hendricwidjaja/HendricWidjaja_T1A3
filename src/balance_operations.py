5eab475eef2c8c5e997708e01aefb899c64e0961
hendricwidjaja <hcwidjaja@gmail.com>
2024-06-30 04:19:54 +1000

    Create draft 'main', 'save_balance' & 'create_balance' functions

--- src/balance_operations.py
+++ src/balance_operations.py
@@ -1,4 +1,7 @@
 import json
+from datetime import datetime
+# Need to import Colorama
+FILE_PATH = "../data/debt_balances.json"
 
 # Load balances from JSON file to access entries
 # Parameter = file_path: Path to JSON file containing entries
@@ -12,13 +15,22 @@ def load_balances(file_path):
     except FileNotFoundError:
         print("File not found. Please check if debt_balances.json file exists")
         return[]
+    except json.JSONDecodeError:
+        print(f"Decoding error detected in balance database (JSON file): {e}")
+        return []
     except Exception as e:
         print(f"Unexpected error occured: {e}")
         return[]
 
+# Save a new balance/entry into the JSON file
+def save_balance(file_path, entries):
+    try:
+        with open(file_path, "w") as file:
+            json.dump(entries, file, indent=4)
+    except Exception as e:
+        print(f"Error. Entry failed to save: {e}")
 
 # View a summary of all balances (if balance names are the same, add all entry amounts)
-
 def balance_summaries(entries):
     # Initialize empty dictionary to contain summary of each balance
     total_balance = {}
@@ -38,5 +50,31 @@ def balance_summaries(entries):
 
 
 # Create a balance (if balance name does not exist, create 1st entry (incl. balance name, entry amount & date))
+def create_balance(entries):
+    print("Please enter the following details")
+    balance_name = input("Balance Name: ")
+    if any(entry["Balance Name"] == balance_name for entry in entries):
+        print("Balance name already exists. Please create a balance with a unique name")
+        return
+    balance_amount = float(input("Balance Amount: "))
+    print("------------------")
+    print("Leaving the date input blank will return today's date")
+    print("------------------")
+    balance_date = input("Date (YYYY-MM-DD):")
+
+    # If balance_date variable holds no value, insert today's date in string format (JSON file cannot hold date values)
+    if not balance_date:
+        balance_date = datetime.today().strftime('%Y-%m-%d')
+
+    # Store new entry in "new_balance" variable
+    new_balance = {"Balance Name": balance_name, "Entry": balance_amount, "Date": balance_date}
+    # Append "new_balance" to entries
+    entries.append(new_balance)
+    # Use save_balance function to rewrite entries (incl. new_balance)
+    save_balance(FILE_PATH, entries)
+    print(f"Your entry for '{balance_name}' has been created successfully.")
+
 
 # Delete a balance (if balance name = "x", delete entry)
+
+# Select a balance (Allow user to view balance history)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,49 @@
 # Main program
 
 #Import functions from operation files
+from balance_operations import load_balances, balance_summaries, save_balance, create_balance
+
+# Determine file path
+FILE_PATH = "../data/debt_balances.json"
 
 # Main program showing options
+def main ():
+    # Use load_balances function to read/load balance entries and store into "entries" variable
+    entries = load_balances(FILE_PATH)
+    # If "entries" is empty, advise the user that there are no balances available and execute "Create balance" function
+    if not entries:
+        print("Debt Tracker 🎯")
+        print("------------------")
+        print("No balances available. Create a new balance")
+        # Insert function to create a balance
+    
+    while True:
+        print("Debt Tracker 🎯")
+        print("------------------")
+
+        balance_summaries(entries)
+
+        print("------------------")
+
+        print("1. Create a balance")
+        print("2. Delete a balance")
+        print("3. Select a balance")
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            create_balance(entries)
+        elif choice == "2":
+            print("Delete balance functionality to be implemented")
+        elif choice == "3":
+            print("Select a balance functionality to be implemented")
+        elif choice == "4":
+            print("Thanks for using Debt Tracker 🎯. Come back soon!")
+            break
+        else:
+            print("Please choose a valid option (1-4)")
+
+
+if __name__ == "__main__":
+    main()
